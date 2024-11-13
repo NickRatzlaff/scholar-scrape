68088422acb82c2f15af8820e22c2c2886bf8f8c
Nick Ratzlaff <nick.ratzlaff@gmail.com>
2024-11-12 21:56:34 -0500

    removed comments

--- AuthorSearch.py
+++ AuthorSearch.py
@@ -29,19 +29,3 @@ table_rows = soup.find_all(class_="gsc_a_tr") #gsc_a_tr
 titles = soup.find_all(class_="gsc_a_at")
 
 print(titles[0])
-
-# #print(response)
-
-# tree = html.fromstring(response)
-# # Extract all paragraph texts
-# table_data = tree.xpath("//tr/td[@class='gsc_a_t']/a/text()") #/text()
-# # for td in table_data:
-# #     print(td)
-
-# table_data_2 = tree.xpath("//tr[@class='gsc_a_tr']")
-
-# for tr in table_data_2:
-#     print(tr.get_text())
-
-
-#print(table)
